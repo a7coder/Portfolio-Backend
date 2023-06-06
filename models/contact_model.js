612766eba8aef2f809fca8467c033923d7725765
A7Coder <a7coder@gmail.com>
2023-06-06 10:43:48 +0530

    Indian Timezone Added

--- models/contact_model.js
+++ models/contact_model.js
@@ -1,12 +1,18 @@
 const mongoose = require("mongoose");
+const moment = require('moment-timezone');
 
 var validateEmail = function (email) {
   var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
   return re.test(email);
 };
 
+moment.tz.add("Asia/Calcutta|HMT BURT IST IST|-5R.k -6u -5u -6u|01232|-18LFR.k 1unn.k HB0 7zX0");
+moment.tz.link("Asia/Calcutta|Asia/Kolkata");
+
+const timezone = moment.tz(Date.now(), "Asia/Calcutta|Asia/Kolkata");
+
 const contact_schema = mongoose.Schema({
-  date: { type: Date, default: Date.now },
+  date: { type: Date, default: timezone },
   name: {
     type: String,
     required: true,
